c82141dafd32253c9b62b632605c2593fd1957c6
PinoRIP <lennardgimmel@gmail.com>
2022-10-02 16:19:56 +0200

    Added empty UE project

---
+++ Source/OrburierProject/OrburierProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OrburierProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ORBURIERPROJECT_API AOrburierProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
