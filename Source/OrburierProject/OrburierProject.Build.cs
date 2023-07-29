b759791cf2d38944609216cc76c8b2094a3b9d5b
PinoRIP <ItsPinoRIP@gmail.com>
2023-07-29 16:58:29 +0200

    Activated Testing plugin

--- Source/OrburierProject/OrburierProject.Build.cs
+++ Source/OrburierProject/OrburierProject.Build.cs
@@ -10,7 +10,7 @@ public class OrburierProject : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange(new string[] {"Orburier", });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
